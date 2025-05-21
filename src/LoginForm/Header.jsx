51cb8275be29a7558f97908d2ce7ad59d3c16ff9
S.Hosein Hoseini <s.hosein.h.82@gmail.com>
2025-05-21 20:58:04 +0330

    Header title fix

--- src/LoginForm/Header.jsx
+++ src/LoginForm/Header.jsx
@@ -2,7 +2,7 @@ import { FaUser } from 'react-icons/fa';
 const Header = () => {
   return (
     <div className="header">
-      <h3>ورود به پنل کاربر</h3>
+      <h3>ورود به پنل کاربری</h3>
       <FaUser className="user-icon" />
     </div>
   );
