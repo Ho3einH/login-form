27a7e4559bb81b2c5d6ff63a51af71e12ce81a47
S.Hosein Hoseini <s.hosein.h.82@gmail.com>
2025-05-21 15:46:16 +0330

    add localStorage in Form

--- src/LoginForm/Form.jsx
+++ src/LoginForm/Form.jsx
@@ -1,18 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from './Header';
 import UserName from './UserName';
 import Password from './Password';
 import LoginBtn from './LoginBtn';
-import SignUpBtn from './SignUpBtn';
-import ForgotPassword from './ForgotPassword';
 const Form = () => {
   const [userName, setUserName] = useState('');
   const [pass, setPass] = useState('');
   const handleSubmit = e => {
     e.preventDefault();
+    localStorage.setItem('username', JSON.stringify(userName));
+    localStorage.setItem('password', JSON.stringify(pass));
     setUserName('');
     setPass('');
   };
+  useEffect(() => {
+    const data = [
+      JSON.parse(localStorage.getItem('username')),
+      JSON.parse(localStorage.getItem('password')),
+    ];
+    console.log(data[0], data[1]);
+    localStorage.clear();
+  }, []);
   return (
     <>
       <form className="container" onSubmit={handleSubmit}>
@@ -23,9 +31,7 @@ const Form = () => {
             onChange={e => setUserName(e.target.value)}
           />
           <Password value={pass} onChange={e => setPass(e.target.value)} />
-          <ForgotPassword />
           <LoginBtn />
-          <SignUpBtn />
         </div>
       </form>
     </>
